055119152dd5cbf59614b6e81b5348a7f876c1b8
Kizito Njoku <nkizito57@gmail.com>
2018-01-31 19:04:07 -0600

    hello firstgit add .

---
+++ JS/index.js
@@ -0,0 +1,20 @@
+function index () {
+    
+    var imgslider = document.getElementById("slideImg");
+    var swiTch = "image2";
+    function slide (){
+        switch(swiTch){
+                case 'image1': imgslider.src = "../Pics/Cat.jpg";
+                swiTch ="image2";
+                break;
+                case 'image2': imgslider.src = "../Pics/Computers.jpg";
+                swiTch ="image3";
+                break;
+                case 'image3': imgslider.src = "../Pics/Girraf.jpg";
+                swiTch = "image1";
+                break;
+        }
+    }
+    
+    setInterval(slide, 6000);
+}
---
+++ Pages/Contact.js
@@ -0,0 +1,26 @@
+function contacts (){
+
+'use strict'; 
+    
+    var submit = document.getElementById("submit");
+    var email = document.getElementsByName("email").value;
+    var message = document.getElementsByName("message").value;
+   
+    function submitting (){
+    window.location.href = "thanks.html";
+    }
+    
+    if (email === "" || message === "") {
+     return false;
+    } else {
+       submit.addEventListener("click", submitting);
+        }
+    
+}
+function goback() {
+function back () {
+    
+     window.location.href = "Contacts.html";
+    }
+    setTimeout(back, 4000);
+}
