53940a1203215c3e5461259bf1b577a43493ff58
Kizito Njoku <nkizito57@gmail.com>
2018-08-05 02:42:30 -0500

    shorter code in slider.js

--- JS/index.js
+++ JS/index.js
@@ -1,29 +1,15 @@
 function index() {
 
     var imgslider = document.getElementById("imgcenter");
-    var swiTch = "image2";
-
-    function slide() {
-        switch (swiTch) {
-            case 'image1':
-                imgslider.style.backgroundImage = "url(../Pics/building.jpg)";
-                swiTch = "image2";
-                break;
-            case 'image2':
-                imgslider.style.backgroundImage = "url(../Pics/Computers.jpg)";
-                swiTch = "image3";
-                break;
-            case 'image3':
-                imgslider.style.backgroundImage = "url(../Pics/Cat.jpg)";
-                swiTch = "image4";
-                break;
-             case 'image4':
-                imgslider.style.backgroundImage = "url(../Pics/clients.jpg)";
-                swiTch = "image1";
-                break;
+    var swiTch = 1;
+    var slid = ["url(../Pics/building.jpg)", "url(../Pics/Computers.jpg)", "url(../Pics/Cat.jpg)", "url(../Pics/clients.jpg)"]
+    
+    setInterval(function() {
+        swiTch++;
+        if(swiTch > slid.length -1){
+            swiTch = 0;
         }
-    }
-
-    setInterval(slide, 6000);
+        imgslider.style.backgroundImage = slid[swiTch];
+    }, 6000);
 
 }
