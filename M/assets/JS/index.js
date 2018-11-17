27448d5aac01f5d5ca19cdd012486d8e0ecf77d2
Kizito Njoku <nkizito57@gmail.com>
2018-11-16 21:30:07 -0600

    added hamburger menu

--- M/assets/JS/index.js
+++ M/assets/JS/index.js
@@ -1,15 +1,46 @@
 function index() {
+  var imgslider = document.getElementById("imgcenter"),
+    swiTch = 1,
+    slid = [
+      "url(assets/images/building.jpg)",
+      "url(assets/images/Computers.jpg)",
+      "url(assets/images/Cat.jpg)",
+      "url(assets/images/clients.jpg)"
+    ];
+  //hamburger Menu
+  $(".hamMenu").click(function() {
+    $("nav ul").slideToggle(500);
+    $(".hamMenu div:nth-child(1)").toggleClass(".first");
+    $(".hamMenu div:nth-child(2)").toggleClass(".second");
+    $(".hamMenu div:nth-child(3)").toggleClass(".third");
+  });
 
-    var imgslider = document.getElementById("imgcenter");
-    var swiTch = 1;
-    var slid = ["url(assets/images/building.jpg)", "url(assets/images/Computers.jpg)", "url(assets/images/Cat.jpg)", "url(assets/images/clients.jpg)"]
-    
-    setInterval(function() {
-        swiTch++;
-        if(swiTch > slid.length -1){
-            swiTch = 0;
-        }
-        imgslider.style.backgroundImage = slid[swiTch];
-    }, 6000);
+  setInterval(function() {
+    swiTch++;
+    if (swiTch > slid.length - 1) {
+      swiTch = 0;
+    }
+    imgslider.style.backgroundImage = slid[swiTch];
+  }, 6000);
+}
 
+function slide() {
+  var imgslider2 = document.getElementById("slidd"),
+    swiTch = 1,
+    images = [
+      "../assets/images/client2.jpg",
+      "../assets/images/bracelet.jpg",
+      "../assets/images/Computers.jpg",
+      "../assets/images/Tv.jpg",
+      "../assets/images/drawing.jpg",
+      "../assets/images/gameConsole.jpg",
+      "../assets/images/Girraf.jpg"
+    ];
+  setInterval(function() {
+    swiTch++;
+    if (swiTch > images.length - 1) {
+      swiTch = 0;
+    }
+    imgslider2.src = images[swiTch];
+  }, 3000);
 }
--- assets/JS/index.js
+++ assets/JS/index.js
@@ -5,7 +5,7 @@ function index() {
 
     var imgslider = document.getElementById("imgcenter");
     var swiTch = 1;
-    var slid = ["url(assets/images/building.jpg)", "url(assets/images/Computers.jpg)", "url(assets/images/Cat.jpg)", "url(assets/images/clients.jpg)"]
+    var slid = ["url(../images/building.jpg)", "url(../images/Computers.jpg)", "url(../images/Cat.jpg)", "url(../images/clients.jpg)"];
     
     setInterval(function() {
         swiTch++;
@@ -15,4 +15,18 @@ function index() {
         imgslider.style.backgroundImage = slid[swiTch];
     }, 6000);
 
+}
+
+function slide() {
+    var imgslider2 = document.getElementById("slidd");
+    var swiTch = 1;
+    var images = ["../assets/images/client2.jpg", "../assets/images/bracelet.jpg", "../assets/images/Computers.jpg", "../assets/images/Tv.jpg", "../assets/images/drawing.jpg", "../assets/images/gameConsole.jpg", "../assets/images/Girraf.jpg"];
+    setInterval(function(){
+        swiTch++;
+        if(swiTch > images.length - 1){
+            swiTch = 0;
+        }
+        imgslider2.src = images[swiTch];
+    }, 3000);
+    
 }
